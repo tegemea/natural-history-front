08a46a3e83fa68aff6dde571b15bcea659d03533
lema <lemajunior@hotmail.com>
2022-02-26 14:30:47 +0300

    added fonts locally, integrated dropdown & working but not animated

--- components/UI/Header.vue
+++ components/UI/Header.vue
@@ -11,8 +11,30 @@
           <ul class="list-unstyled">
             <li><NuxtLink to="/">Home</NuxtLink></li>
             <li><NuxtLink to="#">We Are Natural</NuxtLink></li>
-            <li><NuxtLink to="/what-to-do-when">What to do When</NuxtLink></li>
-            <li><NuxtLink to="/natural-adventures">Natural Adventures</NuxtLink></li>
+            <li @mouseenter="mouseEnter($event)" @mouseleave="mouseLeave($event)">
+              <NuxtLink to="/what-to-do-when">What to do When</NuxtLink>
+              <transition name="fade">
+                <div class="dropdown">
+                  <ul class="dropdown-content">
+                    <li v-for="w in whatToDoWhens" :key="w.id">
+                      <NuxtLink :to="`/what-to-do-when/${w.slug}`">{{ w.name }}</NuxtLink>
+                    </li>
+                  </ul>
+                </div>
+              </transition>
+            </li>
+            <li @mouseenter="mouseEnter($event)" @mouseleave="mouseLeave($event)">
+              <NuxtLink to="/natural-adventures">Natural Adventures</NuxtLink>
+              <transition name="fade">
+                <div class="dropdown">
+                  <ul class="dropdown-content">
+                    <li v-for="n in naturalAdventures" :key="n.id">
+                      <NuxtLink :to="`/natural-adventures/${n.slug}`">{{ n.name }}</NuxtLink>
+                    </li>
+                  </ul>
+                </div>
+              </transition>
+            </li>
             <li><NuxtLink to="#">Why Travel with us</NuxtLink></li>
             <li><NuxtLink to="#">Blog</NuxtLink></li>
             <li><NuxtLink to="/safari-bookings" class="booking mt-3">Safari Booking</NuxtLink></li>
@@ -23,6 +45,68 @@
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      whatToDoWhens: [],
+      naturalAdventures: [],
+    }
+  },
+  async fetch() {
+    const storeHasWhatToDoWhens = this.$store.state.whatToDoWhen.whatToDoWhens.length > 0;
+    const storeHasNaturalAdventures = this.$store.state.naturalAdventures.naturalAdventures.length > 0;
+
+    if(!storeHasWhatToDoWhens) {
+      this.whatToDoWhens = await this.$axios.$get(`${this.$store.state.settings.apiURL}/what-to-do-when`);
+    } else {
+      this.whatToDoWhens = this.$store.state.whatToDoWhen.whatToDoWhens
+    }
+    
+    if(!storeHasNaturalAdventures) {
+      this.naturalAdventures = await this.$axios.$get(`${this.$store.state.settings.apiURL}/natural-adventures`);
+    } else {
+      this.naturalAdventures = this.$store.state.naturalAdventures.naturalAdventures
+    }
+  },
+  methods: {
+    mouseEnter(e) {
+      // console.log(e.target.children[1])
+      e.target.children[1].classList.add('active')
+    },
+    mouseLeave(e) {
+      // console.log(e.target.children[1])
+      e.target.children[1].classList.remove('active')
+    }
+  }
+}
+</script>
+
 <style lang="scss" scoped>
-// 
+.fade-enter {
+  visibility: hidden;
+  opacity: 0;
+  // transform: translateY(-7px);
+}
+
+.fade-enter-active {
+  transition: all .3s;
+  // visibility: visible;
+}
+
+.fade-leave {
+  // 
+}
+
+.fade-leave-active {
+  transition: all .3s;
+  visibility: hidden;
+  opacity: 0;
+  // transform: translateY(-7px);
+}
+
+.active {
+  visibility: visible !important;
+  opacity: 1 !important;
+}
 </style>
--- plugins/scroll.js
+++ plugins/scroll.js
@@ -1,19 +1,12 @@
 (() => {
-  let 
-  scrollV = '', 
-  nav = document.querySelector('#nav'), 
-  logo = document.querySelector('#logo');
-
-  console.log(logo.getAttribute('src'))
+  let scrollV = '', nav = document.querySelector('#nav');
 
   window.onscroll = () => {
     scrollV = window.pageYOffset || document.documentElement.scrollTop;
     if(scrollV > 10) {
       nav.classList.add('scrolled-nav')
-      // logo.setAttribute('src', '../assets/images/logos/transparent-pale.png')
     } else {
       nav.classList.remove('scrolled-nav')
-      // logo.setAttribute('src', '../assets/images/logos/transparent-brand.png')
     }
   }
 })();
